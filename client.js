6aea0979c8065fa951e5a31eb98f8f39a9420d49
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-12 15:47:00 -0700

    Send name using conn.write()

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ const connect = function () {
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log(`Successfully connected to game server`);
+    conn.write("Name: KYN");
   });
 
   conn.on("data", (data) => {
