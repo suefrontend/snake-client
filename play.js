5b4ee027cb55fa3b27d8aca033c41a994852f789
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-12 15:40:34 -0700

    Fix connect function to handle incoming data

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  conn.on("connect", (data) => {
-    // code that does something when the connection is first established
-    console.log(`data: ${data}`);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
