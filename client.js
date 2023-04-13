3543774e1df0da211d0b8147846f653cd26541a6
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-12 17:39:37 -0700

    Add move command

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function () {
     // code that does something when the connection is first established
     console.log(`Successfully connected to game server`);
     conn.write("Name: KYN");
+    conn.write("Move: up");
   });
 
   conn.on("data", (data) => {
