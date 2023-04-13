fa61bacb79fe9089936e3ce1b712e0ad232e1ae6
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-12 19:00:59 -0700

    Separate functions in different files

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); //type something
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  // your code here
+
+  if (data === "\u0003") {
+    console.log("bye");
+    process.exit();
+  }
+};
+
+module.exports = {
+  setupInput,
+};
--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require("./client");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); //type something
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  // your code here
-
-  if (data === "\u0003") {
-    console.log("bye");
-    process.exit();
-  }
-};
-
-setupInput();
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
